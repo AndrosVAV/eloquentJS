b583267579f646e9379895a0b5a4664d0aca4955
AndrosVAV <androsveres@gmail.com>
2021-01-18 18:39:08 +0400

    Начало теория

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
 var str = "Javascript - это круто!!!";
+
+//Выразительный JS(Марейн Хавербеке);
+
+//Теория
+/*
+//тернарный оператор(c.26);
+console.log(true ? 1 : 2);//1
+console.log(false ? 1 : 2);//2
+//Логический оператор(c.);
+console.log(null || "user");//user
+console.log("Karl" || "user");//Karl
+
+console.log(true || x1);//true
+console.log(false && x);//false
+
+console.log(!true);//false
+console.log(!false);//true
+
+var one = 1,two = 2;
+console.log(one + two);
+*/
+//console.log(Math.max(2,4));
+//console.log(Math.min(2,4) + 100);
+
+
+
+
+
+//Условное выполнение
+/*
+var num = prompt("Выбери число");
+if(isNaN(num)){
+    alert("Твоё число - квадратный корень из " + num * num);
+};
+
+
+var num = prompt("Выбери число");
+if(!isNaN(num)){
+    alert("Твоё число - квадратный корень из " + num * num);
+};
+
+
+var num = prompt("Выбери число");
+if(!isNaN(num)){
+    alert("Твоё число - квадратный корень из " + num * num);
+}else{
+    alert("Ну ты что число-то не ввел");
+};
+
+
+
+var num = prompt("Выбери число");
+if(num < 10){
+alert("Маловато");
+}else if(num < 100){
+alert("Нормально");
+}else{
+alert("Многовато");
+};
+
+*/
+
