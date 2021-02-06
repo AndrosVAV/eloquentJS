da4a1b205998469f4bef52f9299483e0fac49afa
AndrosVAV <androsveres@gmail.com>
2021-02-06 14:14:41 +0400

    Циклы

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var str = "Javascript - это круто!!!";
 
 //Выразительный JS(Марейн Хавербеке);
 
-//Теория
+//Теория + повторение
 /*
 //тернарный оператор(c.26);
 console.log(true ? 1 : 2);//1
@@ -60,4 +60,97 @@ alert("Многовато");
 };
 
 */
+
+
+
+
+
+
+
+//ЦИКЛЫ while(c.39)
+/*
+var number = 0;
+while(number <= 12){
+    console.log(number);
+    number = number +=2;
+}
+*/
+
+
+/*
+var counter = 0;
+var result = 1;
+
+while(counter <= 10){
+    result = result *= 2;
+    counter = counter += 1;
+}
+console.log(result);
+*/
+
+//Цикл for(c.42);
+/*
+var result = 1;
+for(var counter = 0;counter <= 10;counter += 1){
+    result *= 2;
+    console.log(result);
+}
+*/
+
+/*
+var result = 1;
+for(var counter = 0;counter <= 10;counter += 1)
+    result *= 2;
+    console.log(result);
+*/
+
+//Выход из цикла(c.43)
+/*
+for(var current = 20;current++){
+    if(current % 7 === 0 && current <= 100){
+        break;
+}
+console.log(current);
+*/
+
+
+/*
+for(var current = 20;current <= 100;current++){
+    if(current % 7 == 0){
+	console.log(current);
+     }
+}
+*/
+
+//switch(c.44);
+/*
+switch(prompt("Как погодка?")){
+    case "дождь" : alert("Не забудь зонт");
+    break;
+    case "снег" : alert("Блин мы в России");
+    break;
+    case "солнечно": alert("Оденься полегче");
+	break;
+	case "облачно" : alert("Иди гуляй");
+	break;
+	default : alert("Непонятная погода");
+	break;
+};
+*/
+
+
+//Упражнения
+
+
+var counter = 0;
+var result = "#";
+//console.log("#");
+while(counter < 8){
+    //result += "#";
+	console.log(result);
+	result += "#";
+    counter++;
+}
+
+
 
